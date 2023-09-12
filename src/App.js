f98c77f050572bc8b0ed9da71b005ae2500f4d45
Amirkhan567 <amrkh5678@gmail.com>
2023-09-12 19:06:13 +0500

    multiple quotes generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(1)
+  const [advice, setAdvice] = useState([
+    "introduction of react js",
+    "learn react js",
+    "invest your new income",
+  ]);
+
+  function nextHandler(){
+    if(counter < 3){
+      setCounter(counter + 1)
+    }
+  }
+
+  function previousHandler(){
+    if(counter > 1){
+      setCounter(counter - 1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{textTransform: "capitalize", fontWeight: "500", backgroundColor: "aliceblue", height: "100vh"}}>
+     <div className="header" style={{display: "flex", justifyContent: "space-around", padding: "40px"}}>
+      {
+        Array.from([1,2,3], (x)=>(
+          <div style={{width: "50px", height: "50px", borderRadius: "50%", backgroundColor: counter >= x ? "rebeccapurple" : "grey", display: "flex", justifyContent: "center", alignItems: "center", color: "white", fontSize: "18px"}}>{x}</div>
+        ))
+      }      
+     </div>
+
+     <div className='advice'>
+      <h1 style={{letterSpacing: "1px",}}>{advice[counter - 1]}</h1>
+      </div>
+
+      <div className='buttons' style={{display: "flex",justifyContent: "space-around", marginTop: "70px",}}> 
+      {
+        Array.from(["previous","next"], (x)=>(
+          <button onClick={x == "next" ? nextHandler : previousHandler} style={{width: "100px", height: "30px", backgroundColor: "rebeccapurple", color: "white", border: "none", borderRadius:"6px", cursor: "pointer",}}>{x}</button>
+        ))
+      }
+      </div>
     </div>
   );
 }
